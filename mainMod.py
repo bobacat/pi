e0b225fabf128ecbaa5c33e3b06333373d5a44fd
brian <brianwadescott@gmail.com>
2019-01-19 10:57:09 -0800

    clean up code

--- mainMod.py
+++ mainMod.py
@@ -1,24 +1,35 @@
 # someshit
 # !/usr/bin/python
 import spidev as SPI                # where the display connects
-import Image, ImageDraw, ImageFont  # PIL - PythonImageLibrary
-import time, datetime, sys, signal, urllib, requests, random
+import Image
+import ImageDraw
+import ImageFont  # PIL - PythonImageLibrary
+import time
+import datetime
+import sys
+import signal
+import urllib
+import requests
+import random
 from StringIO import StringIO
 
 from EPD_driver import EPD_driver
 
+
 def handler(signum, frame):
-    print 'SIGTERM'
+    print('SIGTERM')
     sys.exit(0)
+
+
 signal.signal(signal.SIGTERM, handler)
 random.seed(time.time())
 
 bus = 0
 device = 0
-disp = EPD_driver(spi = SPI.SpiDev(bus, device))
-print "disp size : %dx%d"%(disp.xDot, disp.yDot)
+disp = EPD_driver(spi=SPI.SpiDev(bus, device))
+print("disp size : %dx%d" % (disp.xDot, disp.yDot))
 
-print '------------init and Clear full screen------------'
+print('------------init and Clear full screen------------')
 disp.Dis_Clear_full()
 disp.delay()
 
@@ -26,11 +37,13 @@ disp.delay()
 disp.EPD_init_Part()
 disp.delay()
 
-imagenames = [] # array of tuples ('http://imagelink','description')
+imagenames = []  # array of tuples ('http://imagelink','description')
 
-searchnames = ['Cat', 'Horst', 'Amiga', 'DAC', 'Raspberry', 'diy', 'Acorn', 'Boing']
+searchnames = ['Cat', 'Horst', 'Amiga', 'DAC', 'Raspberry', 'diy', 'Acorn']
 
 # this writes a bunch of image links ad test descriptions to imagenames
+
+
 def querySearchEngine(val):
     search = "http://api.duckduckgo.com/?q="+val+"&format=json&pretty=1"
     req = requests.get(search)
@@ -43,7 +56,7 @@ def querySearchEngine(val):
                         url = topic2["Icon"]["URL"]
                         text = topic2["Text"]
                         if url:
-                            imagenames.append( (url,text) )
+                            imagenames.append((url, text))
                     except:
                         # print topic
                         pass
@@ -51,7 +64,7 @@ def querySearchEngine(val):
                 url = topic["Icon"]["URL"]
                 text = topic["Text"]
                 if url:
-                    imagenames.append( (url,text) )
+                    imagenames.append((url, text))
             except:
                 # print topic
                 pass
@@ -60,6 +73,8 @@ def querySearchEngine(val):
 #    print 'search for', val, ' #entries', len(imagenames) #, imagenames
 
 # function to write the image to the display
+
+
 def imageToDisplay(img):
     # prepare for display
     im = mainimg.transpose(Image.ROTATE_90)
@@ -79,49 +94,50 @@ def imageToDisplay(img):
                     pass
             # print val
             listim2.append(val)
-    for x in range(0,1000):
+    for x in range(0, 1000):
         listim2.append(0)
     # print len(listim2)
     ypos = 0
     xpos = 0
-    disp.EPD_Dis_Part(xpos, xpos+im.size[0]-1, ypos, ypos+im.size[1]-1, listim2) # xStart, xEnd, yStart, yEnd, DisBuffer
-    uploadtime = time.time()
+
+
+disp.EPD_Dis_Part(xpos, xpos+im.size[0]-1, ypos, ypos+im.size[1]-1, listim2)
 
 # font for drawing within PIL
 myfont10 = ImageFont.truetype("amiga_forever/amiga4ever.ttf", 8)
 myfont28 = ImageFont.truetype("amiga_forever/amiga4ever.ttf", 28)
 
 # mainimg is used as screen buffer, all image composing/drawing is done in PIL,
-# the mainimg is then copied to the display (drawing on the disp itself is no fun)
-mainimg = Image.new("1", (296,128))
+# the mainimg is copied to the display (drawing on the disp itself is no fun)
+mainimg = Image.new("1", (296, 128))
 draw = ImageDraw.Draw(mainimg)
 
-skip = 0 # used to slow down image changes, but not clock changes
+skip = 0  # used to slow down image changes, but not clock changes
 while 1:
     starttime = time.time()
     if skip == 0:
-        querySearchEngine(searchnames[random.randint(0,len(searchnames)-1)])
-        imagename = imagenames[random.randint(0,len(imagenames)-1)]
+        querySearchEngine(searchnames[random.randint(0, len(searchnames)-1)])
+        imagename = imagenames[random.randint(0, len(imagenames)-1)]
         # print '---------------------'
         try:
             req = requests.get(imagename[0], stream=True)
             req.raw.decode_content = True
             im = Image.open(StringIO(req.content))
             # print name, im.format, im.size, im.mode
-            im.thumbnail((296,128))
-            im = im.convert("1") #, dither=Image.NONE)
+            im.thumbnail((296, 128))
+            im = im.convert("1")  # dither=Image.NONE)
             # print 'thumbnail', im.format, im.size, im.mode
             loadtime = time.time()
             # print 't:load+resize:', (loadtime - starttime)
 
             # clear
-            draw.rectangle([0,0,296,128], fill=255)
+            draw.rectangle([0, 0, 296, 128], fill=255)
 
             # copy to mainimg
             ypos = (disp.xDot - im.size[1])/2
             xpos = (disp.yDot - im.size[0])/2
             # print 'ypos:', ypos, 'xpos:', xpos
-            mainimg.paste(im, (xpos,ypos))
+            mainimg.paste(im, (xpos, ypos))
 
             # draw info text
             ts = draw.textsize(imagename[1], font=myfont10)
@@ -142,21 +158,21 @@ while 1:
             print 'IOError', str(ex), imagename[0]
             pass
 
-    #draw time
+    # draw time
     now = datetime.datetime.now()
-    tstr = "%02d:%02d:%02d"%(now.hour,now.minute,now.second)
+    tstr = "%02d:%02d:%02d" % (now.hour, now.minute, now.second)
     # draw a shadow, time
     tpx = 36
     tpy = 96
     for i in range(tpy-4, tpy+32, 2):
         draw.line([0, i, 295, i], fill=255)
-    draw.text((tpx-1, tpy  ), tstr, fill=0, font=myfont28)
+    draw.text((tpx-1, tpy), tstr, fill=0, font=myfont28)
     draw.text((tpx-1, tpy-1), tstr, fill=0, font=myfont28)
-    draw.text((tpx  , tpy-1), tstr, fill=0, font=myfont28)
-    draw.text((tpx+2, tpy  ), tstr, fill=0, font=myfont28)
+    draw.text((tpx, tpy-1), tstr, fill=0, font=myfont28)
+    draw.text((tpx+2, tpy), tstr, fill=0, font=myfont28)
     draw.text((tpx+2, tpy+2), tstr, fill=0, font=myfont28)
-    draw.text((tpx  , tpy+2), tstr, fill=0, font=myfont28)
-    draw.text((tpx  , tpy  ), tstr, fill=255, font=myfont28)
+    draw.text((tpx, tpy+2), tstr, fill=0, font=myfont28)
+    draw.text((tpx, tpy), tstr, fill=255, font=myfont28)
     drawtime = time.time()
     # print 't:draw:', (drawtime - loadtime)
 
@@ -166,6 +182,6 @@ while 1:
     # disp.delay()
 
     imageToDisplay(mainimg)
-    skip = (skip+1)%17
+    skip = (skip+1) % 17
 
     # print 't:upload:', (uploadtime - loadtime)
