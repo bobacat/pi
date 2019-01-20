43c234a092953b399616b85e3e7f09a429b53d72
brian <brianwadescott@gmail.com>
2019-01-19 20:59:13 -0800

    next

--- randomStuff.py
+++ randomStuff.py
@@ -11,34 +11,35 @@ import time
 import os
 import random
 
-try:
+epd = epd4in2.EPD()
+epd.init()
+epd.Clear(0xFF)
+
+Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)
+
+
+def choose_random_loading_image(path):
+    images = os.listdir(path)
+    loading_image = random.randint(0, len(images)-1)
+    return path+images[loading_image]
+
+    print("read bmp file")
+    Himage = Image.open('garfield.bmp')
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(2)
+
+
+def main():
     epd = epd4in2.EPD()
     epd.init()
-    epd.Clear(0xFF)
+    # For simplicity, the arguments are explicit numerical coordinates
+    image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)
+    draw = ImageDraw.Draw(Himage)
+    image = Image.open(choose_random_loading_image('bmp/'))
+    Himage = Image.open(image)
+    epd.display(epd.getbuffer(Himage))
+    time.sleep(60)  # change the image every minute
+    main()
 
-    Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)
-    # Drawing on the Vertical image
-    Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 255)
-
-    def choose_random_loading_image(path):
-        images = os.listdir(path)
-        loading_image = random.randint(0, len(images)-1)
-        return path+images[loading_image]
-
-    def main():
-        epd = epd4in2.EPD()
-        epd.init()
-        # For simplicity, the arguments are explicit numerical coordinates
-        image = Image.new('1', (EPD_WIDTH, EPD_HEIGHT), 1)
-        draw = ImageDraw.Draw(Himage)
-        image = Image.open(choose_random_loading_image('bmp/'))
-        epd.display_frame(epd.get_frame_buffer(image))
-        time.sleep(60)  # change the image every minute
+    if __name__ == '__main__':
         main()
-
-        if __name__ == '__main__':
-            main()
-
-    except:
-        print 'traceback.format_exc():\n%s' % traceback.format_exc()
-        exit()
