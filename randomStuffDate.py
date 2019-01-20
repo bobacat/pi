f2c8227d8ee6462ed64910b2a29ae94b822efcb4
brian <brianwadescott@gmail.com>
2019-01-20 11:03:52 -0800

    commit

--- randomStuffDate.py
+++ randomStuffDate.py
@@ -24,21 +24,24 @@ def choose_random_loading_image(path):
     loading_image = random.randint(0, len(images)-1)
     return path+images[loading_image]
 
+    # Drawing on the Horizontal image
+    Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)  # 255: clear the frame
+    # Drawing on the Vertical image
+    Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 255)  # 255: clear the frame
+
+    # Horizontal
+    print "Drawing"
+    draw = ImageDraw.Draw(Himage)
+    font24 = ImageFont.truetype('amiga_forever/amiga4ever.ttf', 24)
+    draw.text((10, 0), 'BOBA IS A BIG WEINER!!', font = font24, fill = 0)
+    draw.text((10, 20), '2.9inch e-Paper', font = font24, fill = 0)
+
 
 def main():
     epd = epd4in2.EPD()
     epd.init()
     # For simplicity, the arguments are explicit numerical coordinates
     image = Image.open(choose_random_loading_image('images/'))
-    now = datetime.datetime.now()
-    currentDate = (now.strftime("%Y-%m-%d"))
-    localtime = (now.strftime("%H:%M:%S"))
-    draw = ImageDraw.Draw(Himage)
-    font24 = ImageFont.truetype('amiga_forever/amiga4ever.ttf', 24)
-    draw.text((10, 0), 'Text', font = font24, fill = 0)
-    draw.text((0, 100), currentDate, font = font24, fill = 0)
-    draw.text((0, 200), localtime, font = font24, fill = 0)
-    epd.display(epd.getbuffer(Himage))
     epd.display(epd.get_frame_buffer(image))
     time.sleep(10)  # change the image every minute
     main()
