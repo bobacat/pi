5378d313a2b53fbcc1399c007be282f72486fc4b
brian <brianwadescott@gmail.com>
2019-01-19 20:34:01 -0800

    fix

--- randomStuff.py
+++ randomStuff.py
@@ -5,23 +5,25 @@ import epd4in2
 import time
 from PIL import Image, ImageDraw, ImageFont
 import traceback
+import Image
+import ImageDraw
+import time
+import os
+import random
 
 try:
     epd = epd4in2.EPD()
     epd.init()
     epd.Clear(0xFF)
 
-
-
-    # Drawing on the Horizontal image
-    Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)  # 255: clear the frame
+    Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)
     # Drawing on the Vertical image
-    Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 255)  # 255: clear the frame
+    Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 255)
 
-def choose_random_loading_image(path):
-    images = os.listdir(path)
-    loading_image = random.randint(0, len(images)-1)
-    return path+images[loading_image]
+    def choose_random_loading_image(path):
+        images = os.listdir(path)
+        loading_image = random.randint(0, len(images)-1)
+        return path+images[loading_image]
 
     def main():
         epd = epd4in2.EPD()
