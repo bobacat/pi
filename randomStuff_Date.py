dc3b72a689e688fb3a4c2083507044b1627e82de
brian <brianwadescott@gmail.com>
2019-01-21 08:18:27 -0800

    commit

--- randomStuff_Date.py
+++ randomStuff_Date.py
@@ -48,7 +48,13 @@ try:
     # Drawing on the Vertical image
     Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 255)
 
-    im = Image.open('images/diehard07.png')
+
+def choose_random_loading_image(path):
+    images=os.listdir(path)
+    loading_image=random.randint(0,len(images)-1)
+    return path+images[loading_image]
+
+    im = Image.open(choose_random_loading_image('images/'))
     font_cal = ImageFont.truetype('amiga_forever/amiga4ever.ttf', 12)
     font_day = ImageFont.truetype('amiga_forever/amiga4ever.ttf', 60)
     font_weather = ImageFont.truetype('amiga_forever/amiga4ever.ttf', 20)
