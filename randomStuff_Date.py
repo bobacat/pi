78d0d01853506d22766eca917e19979e532fbc7f
brian <brianwadescott@gmail.com>
2019-01-21 05:33:00 -0800

    commit

--- randomStuff_Date.py
+++ randomStuff_Date.py
@@ -81,11 +81,11 @@ try:
 
     # The settings for the Calenday today number in the middle
     w_day_num,h_day_num = font_day.getsize(day_number)
-    x_day_num = (cal_width / 2) - (w_day_num / 2) + 20
+    x_day_num = 400 - (cal_width / 2) - (w_day_num / 2)
 
     # The settings for the month string in the middle
     w_month_str,h_month_str = font_month_str.getsize(month_str)
-    x_month_str = (cal_width / 2) - (w_month_str / 2) + 20
+    x_month_str = 400 - (cal_width / 2) - (w_month_str / 2)
 
     draw.rectangle((0, 175, 400, 300), fill=0)  # Calender area rectangle
     draw.text((20, 190),month_cal , font=font_cal, fill = 255)  # Month calender text
