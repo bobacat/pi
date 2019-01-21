b3b47daa7c20096a302540921fbdec3317cabc98
brian <brianwadescott@gmail.com>
2019-01-21 08:04:08 -0800

    commit

--- randomStuff_Date.py
+++ randomStuff_Date.py
@@ -98,9 +98,7 @@ try:
     draw.text((x_day_str, 190),day_str, font=font_day_str, fill = 255)  # Day string calender text
     draw.text((x_day_num, 210),day_number, font=font_day, fill = 255)  # Day number string text
     draw.text((x_month_str, 270),month_str, font=font_month_str, fill = 255)  # Month string text
-    image_black = Image.open('images/diehard07.png')
-    draw_black = ImageDraw.Draw(image_black)
-    draw_black
+    draw.image(im)
     epd.display(epd.getbuffer(Himage))
     time.sleep(2)
     epd.sleep()
