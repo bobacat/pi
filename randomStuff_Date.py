4c967143d26506f57a4b2cbfe255a75944cd209e
brian <brianwadescott@gmail.com>
2019-01-21 11:02:19 -0800

    m

--- randomStuff_Date.py
+++ randomStuff_Date.py
@@ -102,11 +102,12 @@ try:
     draw.text((x_day_num, 210), day_number, font=font_day, fill = 255)  # Day number string text
     draw.text((x_month_str, 270), month_str, font=font_month_str, fill = 255)  # Month string text
 
+    Himage = Image.new()
     bmp = Image.open('100x100.bmp')
     Himage2.paste(bmp, (50,10))
     draw.rectangle((10, 150, 60, 200), fill = 0)
     epd.display(epd.getbuffer(Himage2))
-    
+
 
     epd.display(epd.getbuffer(Himage))
     time.sleep(2)
