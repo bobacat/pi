40de3a29a23c178e5a82208fbd5b19e7978fd0f6
brian <brianwadescott@gmail.com>
2019-01-20 22:47:42 -0800

    commit

--- randomStuff_Date.py
+++ randomStuff_Date.py
@@ -87,7 +87,7 @@ try:
     w_month_str,h_month_str = font_month_str.getsize(month_str)
     x_month_str = (cal_width / 2) - (w_month_str / 2)
 
-    draw.rectangle((0, 250, 400, 0), fill=0)  # Calender area rectangle
+    draw.rectangle((0, 0, 400, 0), fill=0)  # Calender area rectangle
     draw.text((20, 190),month_cal , font=font_cal, fill = 255)  # Month calender text
     draw.text((x_day_str,10),day_str, font=font_day_str, fill = 0)  # Day string calender text
     draw.text((x_day_num,35),day_number, font=font_day, fill = 0)  # Day number string text
