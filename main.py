9d7c726838f5a7eed77ff27eb550b97ccca07f6a
brian <brianwadescott@gmail.com>
2019-01-21 09:03:54 -0800

    commit

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ try:
     epd = epd4in2.EPD()
     epd.init()
     epd.Clear(0xFF)
-    
+
     # Drawing on the Horizontal image
     Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)  # 255: clear the frame
     # Drawing on the Vertical image
     Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 255)  # 255: clear the frame
-    
+
     # Horizontal
     print "Drawing"
     draw = ImageDraw.Draw(Himage)
     font24 = ImageFont.truetype('amiga_forever/amiga4ever.ttf', 24)
     draw.text((10, 0), 'BOBA IS A BIG WEINER!!', font = font24, fill = 0)
     draw.text((10, 20), '2.9inch e-Paper', font = font24, fill = 0)
-    draw.text((150, 0), u'微雪电子', font = font24, fill = 0)    
+    draw.text((150, 0), u'微雪电子', font = font24, fill = 0)
     draw.line((20, 50, 70, 100), fill = 0)
     draw.line((70, 50, 20, 100), fill = 0)
     draw.rectangle((20, 50, 70, 100), outline = 0)
@@ -33,7 +33,7 @@ try:
     draw.chord((200, 50, 250, 100), 0, 360, fill = 0)
     epd.display(epd.getbuffer(Himage))
     time.sleep(2)
-    
+
     # Vertical
     draw = ImageDraw.Draw(Limage)
     font18 = ImageFont.truetype('amiga_forever/amiga4ever.ttf', 18)
@@ -50,22 +50,22 @@ try:
     draw.chord((70, 150, 120, 200), 0, 360, fill = 0)
     epd.display(epd.getbuffer(Limage))
     time.sleep(2)
-    
+
     print "read bmp file"
     Himage = Image.open('4in2.bmp')
     epd.display(epd.getbuffer(Himage))
     time.sleep(2)
-    
+
     print "read bmp file on window"
     Himage2 = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)  # 255: clear the frame
     bmp = Image.open('100x100.bmp')
     Himage2.paste(bmp, (50,10))
+    draw.rectangle((10, 150, 60, 200), fill = 0)
     epd.display(epd.getbuffer(Himage2))
     time.sleep(2)
-        
+
     epd.sleep()
-        
+
 except:
     print 'traceback.format_exc():\n%s' % traceback.format_exc()
     exit()
-
