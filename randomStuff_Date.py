102bfa39ca8d2dc3ee02576e2f69958b16ffa919
brian <brianwadescott@gmail.com>
2019-01-20 20:30:22 -0800

    commit

--- randomStuff_Date.py
+++ randomStuff_Date.py
@@ -16,8 +16,24 @@ epd = epd4in2.EPD()
 epd.init()
 epd.Clear(0xFF)
 
-Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)
-draw = ImageDraw.Draw(Himage)
-font24 = ImageFont.truetype('amiga_forever/amiga4ever.ttf', 24)
-draw.text((10, 0), 'BOBA', font=font24, fill=0)
-draw.text((10, 20), '2.9inch e-Paper', font=font24, fill=0)
+
+def choose_random_loading_image(path):
+    images = os.listdir(path)
+    loading_image = random.randint(0, len(images)-1)
+    return path+images[loading_image]
+
+
+def main():
+    epd = epd4in2.EPD()
+    epd.init()
+    For simplicity, the arguments are explicit numerical coordinates
+    image = Image.open(choose_random_loading_image('images/'))
+    epd.display(epd.get_frame_buffer(image))
+    time.sleep(60)  # change the image every minute
+
+
+    main()
+
+
+if __name__ == '__main__':
+    main()
