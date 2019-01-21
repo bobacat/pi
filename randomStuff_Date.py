0c86013e9d5c3b084bce0514bac64cc64bacef4a
brian <brianwadescott@gmail.com>
2019-01-20 20:02:12 -0800

    commit

--- randomStuff_Date.py
+++ randomStuff_Date.py
@@ -18,23 +18,14 @@ epd.Clear(0xFF)
 
 Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)
 
-
 def choose_random_loading_image(path):
     images = os.listdir(path)
     loading_image = random.randint(0, len(images)-1)
     return path+images[loading_image]
-
-    # Drawing on the Horizontal image
-    Himage = Image.new('1', (epd4in2.EPD_WIDTH, epd4in2.EPD_HEIGHT), 255)  # 255: clear the frame
-    # Drawing on the Vertical image
-    Limage = Image.new('1', (epd4in2.EPD_HEIGHT, epd4in2.EPD_WIDTH), 255)  # 255: clear the frame
-
-    # Horizontal
-    print "Drawing"
     draw = ImageDraw.Draw(Himage)
     font24 = ImageFont.truetype('amiga_forever/amiga4ever.ttf', 24)
-    draw.text((10, 0), 'BOBA IS A BIG WEINER!!', font = font24, fill = 0)
-    draw.text((10, 20), '2.9inch e-Paper', font = font24, fill = 0)
+    draw.text((10, 0), 'BOBA', font=font24, fill=0)
+    draw.text((10, 20), '2.9inch e-Paper', font=font24, fill=0)
 
 
 def main():
@@ -43,7 +34,7 @@ def main():
     # For simplicity, the arguments are explicit numerical coordinates
     image = Image.open(choose_random_loading_image('images/'))
     epd.display(epd.get_frame_buffer(image))
-    time.sleep(10)  # change the image every minute
+    time.sleep(60)  # change the image every minute
     main()
 
 
