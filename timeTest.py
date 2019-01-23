939f98939a40f1ca9595ce4912d1961d4fc1a07a
brian <brianwadescott@gmail.com>
2019-01-22 17:08:58 -0800

    commit

---
+++ timeTest.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import time
+import datetime
+
+now = datetime.datetime.now()
+currentDate = (now.strftime("%Y-%m-%d"))
+localtime = (now.strftime("%H:%M:%S"))
+
+print(now)
